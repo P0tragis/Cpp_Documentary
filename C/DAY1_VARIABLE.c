c622478b14e43da52867da0f69466be7329268ab
Neggabyte <raijenjb@gmail.com>
2025-10-06 21:43:40 +0800

    Add files via upload

--- C/DAY1_VARIABLE.c
+++ C/DAY1_VARIABLE.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 int main() {
     /*
     variable = a reusable container for a value.
@@ -42,11 +42,56 @@ int main() {
     char car[] = "porche";
     char food[] = "tinola";
     char car_model[] = "porche 911 gt";
+    char email[] = "sample@gmail.com";
     printf("your name is %s\n", name);
     printf("you were born in %s\n", month);
     printf("your favorite food is %s\n", food);
     printf("your favorite car brand is %s\n", car);
     printf("the car model you want is %s", car_model);
+    printf("\nyour gmail is %s\n", email);
 
+    //BOOLEAN
+    //INCLUDE <stdbool.h>
+    //bools are binary-either true or false 1,0
+    bool isOnline = true;
+    printf("%d\n",isOnline);
+    
+    //SITUATION TO USE BOOLEAN
+    if (isOnline){
+        printf("you are online,welcome!\n");
+    }
+    else{
+        printf("you are offline\n");
+    }
+
+
+    bool isStudent = false;
+    if (isStudent){
+        printf("You are a student\n");
+    }
+    else{
+        printf("you are not student\n");
+    }
+
+    bool rfid = true;
+    if (rfid){
+        printf("RFID confirmed,please proceed.\n");
+    }
+    else{
+        printf("RFID denied,please try again later.\n");
+    }
+
+
+        //SUMMARY!
+
+        /*
+        variable is a reusable cotainer for a value. it behaves as if it were the value it contains
+        int = whole numbers(4 bytes in a modern systems) [%d]
+        float = single precision decimal number (4 bytes) [%f]
+        double = double precision decimal number(8 bytes)[%lf]
+        char = single character [%c]
+        char[] = array of characters or string [%s]
+        bool = true or false (1 byte,requires <stdbool.h>) [%d]
+        */
     return 0;
 }
